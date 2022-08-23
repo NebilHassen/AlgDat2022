091dd8b38f05576af2fdb73e016364b4dd7eff6d
Nebil Hassen <nebil.hassen@hotmail.com>
2022-08-23 17:28:48 +0200

    Initial commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -7,7 +7,7 @@ public class HelloWorld {
 
         for (int i =0 ; i<=10; i+=2) {
 
-            System.out.println("Vi er på nr " + i);
+            System.out.println("Vi er på nr. " + i);
         }
 
 
