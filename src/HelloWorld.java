621974d41c854f278b7f0f44b390c97976f99599
Nebil Hassen <nebil.hassen@hotmail.com>
2022-08-23 17:23:38 +0200

    Pcen died

---
+++ src/HelloWorld.java
@@ -0,0 +1,15 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("Hello World");
+
+        for (int i =0 ; i<=10; i+=2) {
+
+            System.out.println("Vi er på nr " + i);
+        }
+
+
+    }
+}
